888111eda13fe0060fae9c1e015f3d5d72751d88
Ruhul Amin Parvez <parvez.techforing@gmail.com>
2023-05-13 01:32:42 +0600

    Share Stream Section Done :rocket:

--- pages/homePage/homePage.jsx
+++ pages/homePage/homePage.jsx
@@ -1,18 +1,18 @@
 import MainLayout from "../../components/layouts/MainLayout";
 import CreateMusic from "./createMusic/createMusic";
-import GetStarted from "./getStarted/getStarted";
-import MakeDemo from "./makeDemo/makeDemo";
-import Shop from "./shop/shop";
+// import GetStarted from "./getStarted/getStarted";
+// import MakeDemo from "./makeDemo/makeDemo";
+// import Shop from "./shop/shop";
 import Stream from "./stream/stream";
 
 export default function HomePage() {
   return (
     <MainLayout>
-        <MakeDemo />
+        {/* <MakeDemo /> */}
         <CreateMusic />
         <Stream />
-        <Shop />
-        <GetStarted />
+        {/* <Shop /> */}
+        {/* <GetStarted /> */}
     </MainLayout>
   )
 }
