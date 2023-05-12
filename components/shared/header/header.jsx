942c30d2bcfc7ecb84bc664025d27cf6a53e3f8b
Ruhul Amin Parvez <parvez.techforing@gmail.com>
2023-05-12 10:19:28 +0600

    Header HTML Structure :rocket:

--- components/shared/header/header.jsx
+++ components/shared/header/header.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import headerStyle from './../../../styles/header.module.css';
+import headerStyle from './../../../styles/pages/header.module.css';
 
 const Header = () => {
   return (
