81c5ec6deda7f139afb8873a929b3220ecf9c6a4
Ruhul Amin Parvez <parvez.techforing@gmail.com>
2023-05-19 18:48:37 +0600

    Join Page Web Version Done :octocat:

--- pages/joinPage/joinPage.jsx
+++ pages/joinPage/joinPage.jsx
@@ -2,6 +2,48 @@ import joinStyles from '../../styles/pages/join-page.module.css';
 import Image from 'next/image';
 export default function JoinPage() {
   return (
-   <div>JoinPage</div>
+    <div className={joinStyles.joinPage}>
+      <div className={joinStyles.joinPageContainer}>
+        <div className={joinStyles.joinPageHeading}>
+          <h2><span>Join</span> For Monetization</h2>
+          <p>Want to monetize your voice? we can help.</p>
+        </div>
+        <div className={joinStyles.mainForm}>
+          <div className={joinStyles.joinPageImg}>
+            <Image src="/img/join/join.png" width={570} height={774} alt="" />
+          </div>
+          <div className={joinStyles.joinPageForm}>
+            <form>
+              <div className={joinStyles.formGroup}>
+                <label htmlFor="name">Artist’s Name</label>
+                <input type="text" name="name" id="name" placeholder="Artist’s Name" />
+              </div>
+              <div className={joinStyles.formGroup}>
+                <label htmlFor="email">Email Address</label>
+                <input type="email" name="email" id="email" placeholder="Enter your email" />
+              </div>
+              <div className={joinStyles.formGroup}>
+                <label htmlFor="phone">Mobile Number</label>
+                <input type="text" name="phone" id="phone" placeholder="Enter your number" />
+              </div>
+              <div className={joinStyles.formGroup}>
+                <label htmlFor="facebook-link">Artist's Facebook Link</label>
+                <input type="text" name="facebook-link" id="facebook-link" placeholder="Enter your facebook link" />
+              </div>
+              <div className={joinStyles.formGroup}>
+                <label htmlFor="message">Why do you join with us?</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <div className={joinStyles.formGroup}>
+                <div className={joinStyles.formGroupBtn}>
+                  <button className={joinStyles.btnReset} type="button">Reset</button>
+                  <button className={joinStyles.btnSubmit} type="submit">Submit</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
