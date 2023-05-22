e501e7fe3411351a3b075eb79156d7f790d4cb60
YasinTF <yasin.techforing@gmail.com>
2023-05-22 16:56:47 +0600

    music and subscriptopn pake page done

---
+++ components/shared/AudioPlayer.jsx
@@ -0,0 +1,13 @@
+import a from '../../styles/audioPlayer.module.css';
+
+const AudioPlayer = () => {
+  return (
+    <div className={a.player}>
+      <div className="playerImg">
+        player
+      </div>
+    </div>
+  )
+}
+
+export default AudioPlayer
