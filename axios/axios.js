d8904dbeb37fc73726c4be95c02a7a69d29a9b17
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2023-05-29 23:22:32 +0600

    BaseURL Seperated :rocket:

--- axios/axios.js
+++ axios/axios.js
@@ -1,12 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const baseURL = 'https://api.anydemo.ai/api/v1';
+
 // Async thunk action to handle registration
 export const registerUser = createAsyncThunk(
   'auth/registerUser',
   async (userData, { rejectWithValue }) => {
     try {
-      const response = await axios.post('https://api.anydemo.ai/api/v1/auth/register', userData);
+      const response = await axios.post(`${baseURL}/auth/register`, userData);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
