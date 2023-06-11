e82faf18e403c482a1023799ed5ecabedf14bcf8
YasinTF <eiasin440@gmail.com>
2023-06-11 12:17:27 +0600

    fixing build

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 import './../styles/globals.css';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
-import ReduxStoreProvider from '@/components/provider/ReduxStoreProvider';
+// import ReduxStoreProvider from '@/components/provider/ReduxStoreProvider';
+import { Provider } from 'react-redux';
+import store from '../redux/store/store';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,10 +16,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <ReduxStoreProvider>
+        <Provider store={store}>
           {children}
           <Toaster />
-        </ReduxStoreProvider>
+        </Provider>
       </body>
     </html>
   );
