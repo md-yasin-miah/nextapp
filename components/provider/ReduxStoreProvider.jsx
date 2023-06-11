c0421c7aa23bd47116d64bb4069920fa74ed9114
YasinTF <eiasin440@gmail.com>
2023-06-11 11:01:04 +0600

    fixing build issue

--- components/provider/ReduxStoreProvider.jsx
+++ components/provider/ReduxStoreProvider.jsx
@@ -1,8 +1,8 @@
 'use client';
 
+import store from '@/redux/store/store';
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from '../../redux/store/store';
 
 export default function ReduxStoreProvider({ children }) {
   return <Provider store={store}>{children}</Provider>;
--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
 const nextConfig = {
   images: {
     //for all domains
     domains: ['th.bing.com', '*'],
   },
-  // webpack: (config) => {
-  //   config.resolve.alias['@'] = path.join(__dirname, 'components');
-  //   return config;
-  // },
 }
 
 module.exports = nextConfig
